1c01ff6e71cb8332052908a1866b0cecb3dec011
ArthurUFelix <arthurufelix@gmail.com>
2020-03-11 13:57:11 -0300

    adding custom exception responses

--- backend/src/main/java/com/afelix/ppmtool/services/ProjectService.java
+++ backend/src/main/java/com/afelix/ppmtool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package com.afelix.ppmtool.services;
 
 import com.afelix.ppmtool.domain.Project;
+import com.afelix.ppmtool.exceptions.ProjectIdException;
 import com.afelix.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,9 +13,12 @@ public class ProjectService {
     private ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project) {
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        } catch (Exception e) {
+            throw new ProjectIdException("Identificador do projeto '" + project.getProjectIdentifier().toUpperCase() + "' já existe");
+        }
 
-        // Logic
-
-        return projectRepository.save(project);
     }
 }
